66abf449dea8634372c458968de39c704f7c8a8c
visualstudio <visualstudio@230-PC44>
2019-04-18 14:36:35 -0400

    commit

--- ArrayProcessing/Program.cs
+++ ArrayProcessing/Program.cs
@@ -5,11 +5,30 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ArrayProcessing
-{
-    class Program
+{ 
+    class program
     {
         static void Main(string[] args)
         {
+            int[] arr = { 17, 11, 9, 3, 4, 11, 6, 11, 8, 7 };
+            int arr_size = arr.Length;
+
+            printRepeating(arr, arr_size);
+        }
+        static void printRepeating(int[] arr, int size)
+        {
+            int i, j;
+            Console.Write("elementthatRepeatThreeTimes");
+            for (i = 0; i < size; i++)
+            {
+                for (j = i + 1; j < size; j++)
+
+                {
+                    if (arr[i] == arr[j])
+                        Console.Write(arr[i] + "");
+
+                }
+            }
         }
     }
 }
